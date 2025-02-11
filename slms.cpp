139839aa1037272eeaf25945b0384481629824c4
tecnodeveloper <recluzedev@gmail.com>
2025-02-11 14:43:08 +0500

    slms 2.0

---
+++ slms.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+       cout<<"                            Welcome to Secure Login Management System                            ";
+    return 0;
+}
