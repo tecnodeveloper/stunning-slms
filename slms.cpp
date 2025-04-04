a1b6098a15f30e9fb55a4c0ffe86c142f9a7887e
tecnodeveloper <recluzedev@gmail.com>
2025-04-04 22:36:52 +0500

    commit changes to Microsoft VS CODE

--- slms.cpp
+++ slms.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include"adminpannel.h"
+#include"studentpannel.h"
 using namespace std;
 
 int main()
@@ -8,5 +10,8 @@ int main()
     cout << " -------------------------------- Welcome to Learning Management System -------------------------------- " << endl;
     cout << " ------------------------------------------------------------------------------------------------------- " << endl;
     cout << " ------------------------------------------------------------------------------------------------------- " << endl;
+    cout << "Press 1 for Admin pannel: "<<endl;
+    cout << "Press 2 for Student pannel: " << endl;
+
     return 0;
 }
