38c7b299a4fb77f2221b4a8d2ce2b2d6a386508a
tecnodeveloper <recluzedev@gmail.com>
2025-04-04 12:24:34 +0500

    Update some code Changes 1.0

---
+++ tecnodeveloper/index.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include<string.h>
+using namespace std;
+
+int main() {
+    cout<<" -------------------------------- Welcome to Learning Management System -------------------------------- ";
+    return 0;
+}
