e77985ccf6510f30ed749b80f21bafe8436dd83a
tecnodeveloper <recluzedev@gmail.com>
2025-04-05 22:03:44 +0500

    changes 1.4

--- studentpannel.h
+++ studentpannel.h
@@ -3,9 +3,9 @@ class studentpannel {
 private:
 	string fName;
 	string lName;
-	string fatherName
-		int cnic;
-		int age;
+	string FatherName;
+		int studentCNIC;
+		int Studentage;
 public:
 	string firstName;
 	string lastName;
@@ -17,32 +17,65 @@ public:
 		cout << "Please select only one option: \n 1 for SignIn \n 2 for SignUp";
 		cin >> input;
 	}
-
-	string signUp() {
-
+	if (input == 2)
+	{
+		signUp();
+	}
+	void signUp() {
 		cout << "Please enter your first name: " << endl;
 		cin >> firstName;
 		cout << "Please enter your last name: " << endl;
 		cin >> lastName;
 		cout << "Please enter your father name: " << endl;
 		cin >> FatherName;
+		cout << "Please enter your CNIC No: " << endl;
+		cin >> cnic;
+		cout << "Please enter your Age: " << endl;
+		cin >> age;
 	}
 
 	void setFirstName(string firstName)
 	{
 		fName = firstName;
-
 	}
-	void setFirstName(string lastName)
+	void setLastName(string lastName)
 	{
-		fName = lastName;
-
+		lName = lastName;
 	}
-	void setFirstName(string FatherName)
+	void setFatherName(string FatherName)
 	{
-		fatherName = FatherName;
-
+		FatherName = fatherName;
+	}
+	void setCnic(int cnic)
+	{
+		studentCNIC = cnic;
+	}
+	void setAge(int age)
+	{
+		Studentage = age;
 	}
 
+
+
+	string getFirstName()
+	{
+		return firstName;
+	}
+	string getLastName()
+	{
+		return lastName;
+	}
+	string getFirstName()
+	{
+		return FatherName;
+	}
+	int getCnic()
+	{
+		return cnic;
+	}
+	int getAge()
+	{
+		return age;
+	}
 
 };
