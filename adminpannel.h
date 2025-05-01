23a7a1c174cdcaf3f837706e44d8d9892085961d
tecnodeveloper <recluzedev@gmail.com>
2025-05-01 16:00:35 +0500

    1-- commit 3

---
+++ adminpannel.h
@@ -0,0 +1,12 @@
+using namespace std;
+class adminpannel {
+private:
+	string adminName;
+	public:
+		string name;
+		adminpannel()
+		{
+			cout << "Construction of admin pannel is still on working. Please be patient" << endl; ;
+		}
+
+};
---
+++ slms.cpp
@@ -0,0 +1,45 @@
+// #include <iostream>
+// // #include"adminpannel.h"
+// // #include"studentpannel.h"
+// using namespace std;
+//
+// int main()
+// {
+//     int input{ 0 };
+//     cout << " ------------------------------------------------------------------------------------------------------- " << endl;
+//     cout << " ------------------------------------------------------------------------------------------------------- " << endl;
+//     cout << " -------------------------------- Welcome to Learning Management System -------------------------------- " << endl;
+//     cout << " ------------------------------------------------------------------------------------------------------- " << endl;
+//     cout << " ------------------------------------------------------------------------------------------------------- " << endl;
+//     // do
+//     // {
+//         cout << "Press 1 for Admin panel \t \t \t Press 2 for Student panel: " << endl;
+//         cin >> input;
+//     //     if (input == 1 || input == 2)
+//     //     {
+//     //         if (input == 1)
+//     //         {
+//     //             // adminpannel adminobj;
+//     //         }
+//     //         else if(input == 2)
+//     //         {
+//     //             // studentpannel studentobj;
+//     //         }
+//     //         break;
+//     //
+//     //     }
+//     //     else
+//     //     {
+//     //
+//     //         cout << "Try again! Press only 1 or 2 " << endl;
+//     //     }
+//     // } while (true);
+//     return 0;
+// }
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"hello world";
+    return 0;
+}
