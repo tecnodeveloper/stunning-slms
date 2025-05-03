5d855d9e7d534f87093d0e16c5e1ed9ee307c0bb
Zain Ali <recluzedev@gmail.com>
2025-05-03 09:25:25 +0500

    3-- commit 3

---
+++ main.cpp
@@ -0,0 +1,21 @@
+ #include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Welcome to learning management System";
+    cout<<"Press 1 for sign up or 2 for login";
+    cin>>num;
+    if (num == 1)
+    {
+        string name;
+        string rollno;
+        string email;
+        cout<<"Enter your name: ";
+        cin>>name;
+        cout<<"Enter your rollno: ";
+        cin>>rollno;
+        cout<<"Enter your email: ";
+        cin>>email;
+
+    return 0;
+}
