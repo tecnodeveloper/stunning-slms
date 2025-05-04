af51a33c89768c4c28fc0f4dff00baead16b0b46
Zain Ali <recluzedev@gmail.com>
2025-05-04 06:36:00 +0500

    4-- commit 4

--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
 #include<iostream>
 #include<string>
+#include <fstream>
 using namespace std;
 int main()
 {
-    int num{0};
-    string Fullname;
-    int rollno;
-    string email;
+    int num{0}, rollno,passd{0};
+    string Fullname,email;
 
     cout<<"Welcome to learning management System \n";
     cout<<"Press 1 for sign up or 2 for login: \n";
@@ -16,16 +15,23 @@ int main()
         cin.ignore();
         cout<<"Enter your fullname: "<<endl;
         getline(cin, Fullname);
-
         cout<<"Enter your rollno:"<<endl;
         cin>>rollno;
         cout<<"Enter your email address: "<<endl;
         cin>>email;
-
-        // cin>>Fullname;
+        cout<<"Create password: ";
+        cin>>passd;
+        ifstream a_file ("filename.txt");
         cout<<Fullname<<endl;
         cout<<rollno<<endl;
         cout<<email<<endl;
+
+    }
+    else if (num == 2) {
+
+    }
+    else {
+        cout<<"The numbers is't correct please choose 1 or 2";
     }
     return 0;
 }
