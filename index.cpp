6ea635428c874d680e6a4a744e979f425a5e3dc7
tecnodeveloper <recluzedev@gmail.com>
2025-07-02 05:18:21 +0500

    added class implementation with struct and class

--- index.cpp
+++ index.cpp
@@ -1,9 +1,88 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+// #define Struct_Implementation
+#define Class_Implementation
 using namespace std;
+//
+//
+class LMS {
+public:
+  // Setter & Getter functions
+  void setName(string name) { Name = name; }
+  void setFName(string fname) { fName = fname; }
+  void setCNIC(int cnic) { CNIC = cnic; }
+  void setEmail(string email) { Email = email; }
+  void setPassword(int password) { Password = password; }
+  string getName() { return Name; }
+  string getFname() { return fName; }
+  int getCNIC() { return CNIC; }
+  string email() { return Email; }
+  int getPassword() { return Password; }
+
+  //
+  //
+  //
+  void NewUser() {
+    // Write data
+    ofstream data_write;
+    data_write.open("NewUser.bin");
+    data_write << Name << endl;
+    data_write << fName << endl;
+    data_write << CNIC << endl;
+    data_write << Email << endl;
+    data_write << Password << endl;
+    data_write.close();
+
+    // Read data
+    ifstream data_read;
+    data_read.open("NewUser.bin");
+    string dummyLine, file_email, file_password_str;
+    int file_password;
+    while (getline(data_read, dummyLine)) {
+      if (getline(data_read, dummyLine))
+        if (getline(data_read, dummyLine))
+          if (getline(data_read, file_email))
+            if (getline(data_read, file_password_str))
+              file_password = stoi(file_password_str);
+      {
+        cout << "\n\nYou have successfully store your information....... ";
+      }
+
+      {
+      }
+    }
+  }
+  //
+  //
+  //
+  void Sign_In() {
+    string signIn_email;
+    int signIn_password;
+
+    cout << "\n\n   --------------- SignIn Page -----------------  ";
+
+    cout << "\nEnter your email address: ";
+    cin >> signIn_email;
+    cin.clear();
+    cout << "\nEnter your password: ";
+    cin >> signIn_password;
+    if (Email == signIn_email && Password == signIn_password) {
+      cout << "\nYou have Successfully Login ";
+    } else if (signIn_email == "admin" && signIn_password == 12345) {
+      cout << "\nYou have Successfully Login as Admin ";
+    }
+  }
+
+private:
+  string Name, fName, Email;
+  int CNIC, Password;
+};
+//
+//
 
 int main() {
+#ifdef Struct_Implementation
   string fullname, fatherName, email;
   int cnicId, passd;
   cout << "    ----------SignUp Page----------      ";
@@ -68,6 +147,43 @@ int main() {
             }
     }
   }
+#endif
+//
+//
+#ifdef Class_Implementation
+  // Making link b/w class
+  LMS student_Zain;
+  //
+  //
+  string name, fname, email;
+  int CNIC{0}, paswd{0};
+  cout << "   --------------- SignUp Page -----------------  ";
+  cout << "\nEnter your Full name: ";
+  getline(cin, name);
+  cout << "\nEnter your Father name: ";
+  getline(cin, fname);
+  cout << "\nEnter your CNIC number: ";
+  cin >> CNIC;
+  cin.clear();
+  cout << "\nEnter your email address: ";
+  cin >> email;
+  cin.clear();
+  cout << "\nEnter your password: ";
+  cin >> paswd;
+  cin.clear();
+  //
+  //
+  student_Zain.setName(name);
+  student_Zain.setFName(fname);
+  student_Zain.setCNIC(CNIC);
+  student_Zain.setEmail(email);
+  student_Zain.setPassword(paswd);
+  //
+  //
+
+  student_Zain.NewUser();
+  student_Zain.Sign_In();
+#endif
 
   return 0;
 }
